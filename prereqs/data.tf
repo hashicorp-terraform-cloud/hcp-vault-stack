b66824749122dd35266ffb52bf656e375c993225
Ben Holmes <ben.holmes@hashicorp.com>
2024-05-31 17:27:31 +0100

    updated prereqs to pull k8s data

--- prereqs/data.tf
+++ prereqs/data.tf
@@ -2,3 +2,13 @@ data "hcp_vault_secrets_secret" "tfe_token" {
   app_name    = "Stacks"
   secret_name = "TF_STACKS_TFE_TOKEN"
 }
+
+data "hcp_vault_secrets_secret" "kubernetes_ca_bundle" {
+  app_name    = "Stacks"
+  secret_name = "KUBERNETES_CA_BUNDLE"
+}
+
+data "hcp_vault_secrets_secret" "kubernetes_token_reviewer_jwt" {
+  app_name    = "Stacks"
+  secret_name = "KUBERNETES_TOKEN_REVIEWER_JWT"
+}
--- prereqs/outputs.tf
+++ prereqs/outputs.tf
@@ -6,3 +6,13 @@ output "tfe_token" {
   value     = data.hcp_vault_secrets_secret.tfe_token.secret_value
   sensitive = true
 }
+
+output "kubernetes_ca_bundle" {
+  value = data.hcp_vault_secrets_secret.kubernetes_ca_bundle.secret_value
+  sensitive = true
+}
+
+output "kubernetes_token_reviewer_jwt" {
+  value = data.hcp_vault_secrets_secret.kubernetes_token_reviewer_jwt.secret_value
+  sensitive = true
+}
